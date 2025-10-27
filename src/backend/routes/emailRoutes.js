3a28cdc9fe690fced1e05f19298d2bc6e39132df
Jhanok Leon <1649297@senati.pe>
2025-10-27 11:14:15 -0500

    avance 2

--- src/backend/routes/emailRoutes.js
+++ src/backend/routes/emailRoutes.js
@@ -1,3 +1,33 @@
 import express from 'express';
-import { emailController } from '../controllers/emailController.js';
+import { registerUser } from '../controllers/emailController.js';
 
+const router = express.Router();
+/**
+ * @swagger
+ * /api/email/register:
+ *   post:
+ *     summary: Registra usuario y envía correo
+ *     tags: [Email]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: "miguelpi@gmail.com"
+ *               name:
+ *                 type: string
+ *                 example: "miguel"
+ *     responses:
+ *       201:
+ *         description: Email enviado correctamente
+ *       500:
+ *         description: Error del servidor al enviar email
+ */
+
+router.post("/register", registerUser)
+
+export default router;
