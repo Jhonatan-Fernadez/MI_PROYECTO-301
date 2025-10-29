4e473d4698b7f5f721206dda088c5b4e60377475
Jhanok Leon <1649297@senati.pe>
2025-10-29 11:18:06 -0500

    frontend

---
+++ src/frontend/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="min-h-screen bg-gray-100 pl-8">
+      <h1 className="text-2xl font-boald mb-4">soy jhinatan</h1>
+      <p1 className="text-orange-600 text-xl">Esta es mi pagina web</p1>
+    </div>
+  );
+}
+export default Home;
