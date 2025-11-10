9282e6356dbff80c9e3a7257256580cb42d54eee
Jhanok Leon <1649297@senati.pe>
2025-11-10 07:28:13 -0500

    actualizacion de frontend

--- src/frontend/tailwind.config.js
+++ src/frontend/tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        primary: "#46c768",
+        secondary: "#ff7849",
+      },
       fontFamily: {
-        sans: ["Quicksand", "sans-serif"],
+        sans: ["Bitcount Grid Single", "sans-serif"],
       },
     },
   },
